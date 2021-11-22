e70ddd0a44e58bf4be8245289560c16f646af9b9
hojuly <hojune.choi.me@gmail.com>
2021-11-22 14:07:08 +0900

    PERCENT - int / int

---
+++ sql_recipe/chaper3/percent.sql
@@ -0,0 +1,41 @@
+-- 값 비율 계산
+DROP TABLE IF EXISTS advertising_stats;
+CREATE TABLE advertising_stats (
+    dt          varchar(255)
+  , ad_id       varchar(255)
+  , impressions integer
+  , clicks      integer
+);
+
+INSERT INTO advertising_stats
+VALUES
+    ('2017-04-01', '001', 100000,  3000)
+  , ('2017-04-01', '002', 120000,  1200)
+  , ('2017-04-01', '003', 500000, 10000)
+  , ('2017-04-02', '001',      0,     0)
+  , ('2017-04-02', '002', 130000,  1400)
+  , ('2017-04-02', '003', 620000, 15000)
+;
+
+/*
+-- 정수 자료형 데이터 나누기
+*/
+SELECT
+    dt
+    , ad_id
+    , clicks / impressions AS ctr
+    , 100 * clicks / impressions AS ctr_as_percent
+FROM
+    advertising_stats
+WHERE 
+    dt = '2017-04-01'
+ORDER BY
+    dt, ad_id
+;
+-- +------------+-------+--------+----------------+
+-- | dt         | ad_id | ctr    | ctr_as_percent |
+-- +------------+-------+--------+----------------+
+-- | 2017-04-01 | 001   | 0.0300 |         3.0000 |
+-- | 2017-04-01 | 002   | 0.0100 |         1.0000 |
+-- | 2017-04-01 | 003   | 0.0200 |         2.0000 |
+-- +------------+-------+--------+----------------+
