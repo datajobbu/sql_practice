04e5ba1cfff8841e9623ced1cd375f46ee779c02
hojuly <hojune.choi.me@gmail.com>
2021-11-22 13:28:27 +0900

    COMPARE - SIGN(), CASE()

---
+++ sql_recipe/chaper3/compare.sql
@@ -0,0 +1,43 @@
+-- 여러 개의 값 비교
+DROP TABLE IF EXISTS quarterly_sales;
+CREATE TABLE quarterly_sales (
+    year integer
+  , q1   integer
+  , q2   integer
+  , q3   integer
+  , q4   integer
+);
+
+INSERT INTO quarterly_sales
+VALUES
+    (2015, 82000, 83000, 78000, 83000)
+  , (2016, 85000, 85000, 80000, 81000)
+  , (2017, 92000, 81000, NULL , NULL )
+;
+
+/*
+-- q1, q2 컬럼 비교
+*/
+SELECT
+    year
+    , q1
+    , q2
+    , CASE
+        WHEN q1 < q2 THEN '+'
+        WHEN q1 = q2 THEN ' '
+        ELSE '-'
+    END AS judge_q1_q2
+    , q2 - q1 AS diff_q2_q1
+    , SIGN(q2 - q1) AS sign_q2_q1
+FROM
+    quarterly_sales
+ORDER BY
+    year
+;
+-- +------+-------+-------+-------------+------------+------------+
+-- | year | q1    | q2    | judge_q1_q2 | diff_q2_q1 | sign_q2_q1 |
+-- +------+-------+-------+-------------+------------+------------+
+-- | 2015 | 82000 | 83000 | +           |       1000 |          1 |
+-- | 2016 | 85000 | 85000 |             |          0 |          0 |
+-- | 2017 | 92000 | 81000 | -           |     -11000 |         -1 |
+-- +------+-------+-------+-------------+------------+------------+
