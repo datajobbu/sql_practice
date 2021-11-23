cf99707c6ad66d68af1d93fbcadbc24920059c1d
hojuly <hojune.choi.me@gmail.com>
2021-11-23 09:41:37 +0900

    DIST - Euclidean

--- sql_recipe/chaper3/distance.sql
+++ sql_recipe/chaper3/distance.sql
@@ -47,3 +47,12 @@ FROM
 -- |    0 |    0 |
 -- |    1 |    1 |
 -- +------+------+
+
+/*
+-- Euclidean Distance
+*/
+SELECT
+    SQRT(POWER(x1 - x2, 2) + POWER(y1 - y2, 2)) AS dist
+FROM
+    location_2d
+;
