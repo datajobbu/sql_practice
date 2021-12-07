96d295bb3c41070634f8141935eabdd7c52fdd50
hojuly <hojune.choi.me@gmail.com>
2021-12-07 16:24:55 +0900

    MULTI - CTE WITH JOIN

--- sql_recipe/chaper3/multi_table_cte.sql
+++ sql_recipe/chaper3/multi_table_cte.sql
@@ -57,3 +57,53 @@ FROM
 -- | dvd           | D002       | 20000 |     2 |
 -- | dvd           | D003       | 10000 |     3 |
 -- +---------------+------------+-------+-------+
+
+/*
+-- Category Rank Result As Horizontal
+-- CTE WITH JOIN */
+WITH
+    product_sale_ranking AS (
+        SELECT
+            category_name
+            , product_id
+            , sales
+            , ROW_NUMBER() OVER(
+                PARTITION BY category_name
+                ORDER BY sales
+                DESC
+             ) AS ranks
+        FROM
+            product_sales
+    )
+    , mst_rank AS (
+        SELECT DISTINCT
+            ranks
+        FROM
+            product_sale_ranking
+    )
+
+SELECT
+    m.ranks
+    , r1.product_id AS dvd
+    , r1.sales AS dvd_sales
+    , r2.product_id AS cd
+    , r2.sales AS cd_sales
+    , r3.product_id AS book
+    , r3.sales AS book_sales
+FROM
+    mst_rank AS m
+    LEFT JOIN
+        product_sale_ranking AS r1
+        ON m.ranks = r1.ranks
+        AND r1.category_name = 'dvd'
+    LEFT JOIN
+        product_sale_ranking AS r2
+        ON m.ranks = r2.ranks
+        AND r2.category_name = 'cd'
+    LEFT JOIN
+        product_sale_ranking AS r3
+        ON m.ranks = r3.ranks
+        AND r3.category_name = 'book'
+ORDER BY
+    m.ranks
+;
